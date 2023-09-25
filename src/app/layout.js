cc65fdfd5ef1fc111808d6809c309123e78381b7
ggrossi <grossigus@gmail.com>
2023-09-25 14:56:10 -0300

    installed twitter pixel

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,6 +34,15 @@ export default function RootLayout({ children }) {
         <meta property="twitter:title" content={metadata.title} />
         <meta property="twitter:description" content={metadata.description} />
         <meta property="twitter:image" content={metadata.image} />
+
+        {/* Twitter Pixel Code */}
+        <script>
+        !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+        },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+        a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+        twq('config','ofnig');
+        </script>
+
       </head>
       <body className="font-sans" style={{ fontFamily: 'League Spartan' }}>
         {children}
