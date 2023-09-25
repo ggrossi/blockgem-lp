9038d90d85c5d7be309e595571592d7ded19f09c
ggrossi <grossigus@gmail.com>
2023-09-25 02:16:59 -0300

    Redirect to Telegram

--- src/app/components/LeftSection.js
+++ src/app/components/LeftSection.js
@@ -3,10 +3,9 @@ import React, { useRef, useState } from 'react';
 
 function LeftSection() {
   const emailRef = useRef();
-  const [successMessage, setSuccessMessage] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
-  const subscribeUser = async (e) => {
+  const subscribeAndRedirect = async (e, redirectUrl) => {
     e.preventDefault();
     const email = emailRef.current.value;
     try {
@@ -21,12 +20,11 @@ function LeftSection() {
       });
       
       if (res.status === 200) {
-        setSuccessMessage('Successfully subscribed!');
         setErrorMessage(''); // Clear any previous error messages
+        window.location.href = redirectUrl; // Redirect to the respective Telegram link
       } else {
         const data = await res.json();
         setErrorMessage(data.error || 'Something went wrong!');
-        setSuccessMessage(''); // Clear any previous success messages
       }
     } catch (error) {
       setErrorMessage('An error occurred while subscribing!');
@@ -39,13 +37,12 @@ function LeftSection() {
       <h2 className="text-5xl md:text-6xl font-extrabold overflow-hidden">Unlock the Power of Crypto Arbitrage</h2>
       <p className="text-lg mt-4 overflow-hidden">Our innovative Telegram bot covers <span className="font-bold">over 100+ crypto assets and providing real-time prices from 75+ DEXes and CEXes</span>.</p>
       <p className="text-lg mt-4 overflow-hidden">Subscribe now to elevate your trading experience with our exclusive, user-friendly features!</p>
-      {successMessage && <p className="text-green-500">{successMessage}</p>}
       {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-      <form onSubmit={subscribeUser}>
+      <form>
         <input type="email" placeholder="Your Best Email" className="mt-4 p-2 rounded-md w-full text-zinc-400" ref={emailRef} required />
         <div className="mt-4 flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
-          <button type="submit" className="flex items-center justify-center px-4 py-2 rounded-md bg-blue-500 hover:bg-blue-600 text-white w-full md:flex-1">Launch Telegram Bot</button>
-          <button type="submit" className="flex items-center justify-center px-4 py-2 rounded-md bg-blue-500 hover:bg-blue-600 text-white w-full md:flex-1">Join Telegram Channel</button>
+          <button onClick={(e) => subscribeAndRedirect(e, 'https://t.me/blockgemarb_bot')} className="flex items-center justify-center px-4 py-2 rounded-md bg-blue-500 hover:bg-blue-600 text-white w-full md:flex-1">Launch Telegram Bot</button>
+          <button onClick={(e) => subscribeAndRedirect(e, 'https://t.me/+MViTUDTB845hYzIx')} className="flex items-center justify-center px-4 py-2 rounded-md bg-blue-500 hover:bg-blue-600 text-white w-full md:flex-1">Join Telegram Channel</button>
         </div>
       </form>
     </div>
